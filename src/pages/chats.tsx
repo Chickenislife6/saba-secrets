2650523a80c24b42c56a4e492ba0f7d3fafdd654
Jemin Park <jemin.park@outlook.com>
2023-04-07 00:10:09 -0400

    Updated schema with Signal keys

---
+++ src/pages/chats.tsx
@@ -0,0 +1,7 @@
+/* 
+    TODO: Chats page
+    Primary Components: ChatList, Chat, Navbar/Sidebar
+    Secondary Components: 
+        ChatList: ChatListHeader, ChatListRow 
+        Chat: ChatHeader, ChatMessage, ChatInput
+*/
