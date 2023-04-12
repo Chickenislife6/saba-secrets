faf8475d54fb2b801e1b887818eba0c75ed55575
Jemin Park <jemin.park@outlook.com>
2023-04-12 01:03:04 -0400

    Swap for web crypto api for collision resistant key ids

--- src/utils/user/user-keys.ts
+++ src/utils/user/user-keys.ts
@@ -5,18 +5,13 @@ import type { UserKeys, PublicUserKeys } from './types'
  * Creates a new set of user keys via libsignal's KeyHelper
  */
 export async function createNewUserKeys() {
-  // Registration ID is a number identifies the user, can be same as userId
-  // A DB-based registration/user id is better, as it can enforce the constraint
-  // const registrationId = KeyHelper.generateRegistrationId()
-
   const identityKeyPair = await KeyHelper.generateIdentityKeyPair()
 
-  // TODO: make key ids collision resistant
-  const signedPreKeyId = Math.floor(10e8 * Math.random())
-  const signedPreKey = await KeyHelper.generateSignedPreKey(identityKeyPair, signedPreKeyId)
+  const keyIds = crypto.getRandomValues(new Uint32Array(2))
 
-  const oneTimePreKeyId = Math.floor(10e8 * Math.random())
-  const oneTimePreKey = await KeyHelper.generatePreKey(oneTimePreKeyId)
+  const signedPreKey = await KeyHelper.generateSignedPreKey(identityKeyPair, keyIds[0]!)
+
+  const oneTimePreKey = await KeyHelper.generatePreKey(keyIds[1]!)
 
   return {
     identityKeyPair,
