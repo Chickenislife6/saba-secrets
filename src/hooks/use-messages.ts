ec3ca2567d506f64870eb4a8d1fa33563fe810e7
Jemin Park <jemin.park@outlook.com>
2023-04-20 18:21:16 -0400

    Fix message changes

--- src/hooks/use-messages.ts
+++ src/hooks/use-messages.ts
@@ -3,20 +3,17 @@ import { api } from '~/utils/api'
 export function useMessages() {
   // load local storage here
 
-  const newMessages = api.messages.loadNewMessages.useQuery(
-    {},
-    {
-      staleTime: Infinity,
-    }
-  )
+  const newMessages = api.messages.loadNewMessages.useQuery(undefined, {
+    staleTime: Infinity,
+  })
+
+  // subscribe to postgres updates
 
   // use state to preserve messages in store
 
   // memoize grouped messages, or set as an outside object store?
   // this is why we consider zustand
   // const groupedMessages = useMemo(() => { },
-
-  // subscribe to wss channel
 
   // save to local storage
   return newMessages
